4b81e6671e02c5782c61035b83012fe11fb204c7
rececoffin <rece.coffin@connexta.com>
2019-04-30 10:10:46 -0600

    Added stuff

--- code.py
+++ code.py
@@ -1,10 +1,21 @@
 import subprocess
-p = subprocess.Popen("roscore", stdout=subprocess.PIPE, shell=True)
-#change to camera that is used 
-p = subprocess.Popen("rosrun uvc_camera uvc_camera_node &", stdout=subprocess.PIPE, shell=True)
-p = subprocess.Popen("rosrun find_object_2d find_object_2d image:=image_raw", stdout=subprocess.PIPE, shell=True)
-p = subprocess.Popen("rosrun find_object_2d print_objects_detected", stdout=subprocess.PIPE, shell=True)
-(output, err) = p.communicate()
-file_object  = open("file.txt","w")
-file_object.write(output)
-#might have to translates positions to take start into account
+def countPoint():
+	p = subprocess.run(['timeout', '2s', 'rosrun', 'find_object_2d', 'find_object_2d', 'image:=/camera/rgb/image_raw', '>', 'test.txt'])
+	text_file = open("test.txt", "r")
+	count = 0
+	while(True):
+		line = text_file.readline()
+		x = line.split()
+		breakTime = False
+		for i in range (0, len(x)):
+			if(x[i]=="descriptors"):
+				if(x[i-1]!="Extracting"):
+					count= int(x[i-1])
+					breakTime = True
+					break
+		if(breakTime == True):
+			break
+	return count
+
+test = countPoint()
+print(test)
--- patrol.py
+++ patrol.py
@@ -1,32 +1,13 @@
 #!/usr/bin/env python
 
-'''
-Copyright (c) 2015, Mark Silliman
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
-
-# TurtleBot must have minimal.launch & amcl_demo.launch
-# running prior to starting this script
-# For simulation: launch gazebo world & amcl_demo prior to run this script
-
-import rospy
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
 from geometry_msgs.msg import Pose, Point, Quaternion
 import sys
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-class GoToPose():
+class navigate():
     def __init__(self):
 
         self.goal_sent = False
@@ -61,7 +42,6 @@ class GoToPose():
         result = False
 
         if success and state == GoalStatus.SUCCEEDED:
-            # We made it!
             result = True
         else:
             self.move_base.cancel_goal()
@@ -76,27 +56,24 @@ class GoToPose():
         rospy.sleep(1)
 
 if __name__ == '__main__':
-    x = sys.argv[1]
-    y = sys.argv[2]
     try:
         rospy.init_node('nav_test', anonymous=False)
-        navigator = GoToPose()
+        navigator = nagivate()
 
-        # Customize the following values so they are appropriate for your location
+	# Change these values for different locations on the map
         position = {'x': 1.22, 'y' : 2.56}
         quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
 
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
         success = navigator.goto(position, quaternion)
 
         if success:
-            rospy.loginfo("Hooray, reached the desired pose")
+            rospy.loginfo("Reached destination")
         else:
-            rospy.loginfo("The base failed to reach the desired pose")
+            rospy.loginfo("Failed to reach destination")
 
         # Sleep to give the last log messages time to be sent
         rospy.sleep(1)
 
     except rospy.ROSInterruptException:
-        rospy.loginfo("Ctrl-C caught. Quitting")
+        rospy.loginfo("Ctrl-C, Quitting")
 
