f95c7836e756239e087fb92c3637cb6c0f3e3541
Rece Coffin <rece.coffin@connexta.com>
2019-04-30 12:04:31 -0600

    added object count to file

--- code.py
+++ code.py
@@ -1,21 +1,21 @@
 import subprocess
 def countPoint():
-	p = subprocess.run(['timeout', '2s', 'rosrun', 'find_object_2d', 'find_object_2d', 'image:=/camera/rgb/image_raw', '>', 'test.txt'])
-	text_file = open("test.txt", "r")
-	count = 0
-	while(True):
-		line = text_file.readline()
-		x = line.split()
-		breakTime = False
-		for i in range (0, len(x)):
-			if(x[i]=="descriptors"):
-				if(x[i-1]!="Extracting"):
-					count= int(x[i-1])
-					breakTime = True
-					break
-		if(breakTime == True):
-			break
-	return count
+    p = subprocess.run(['timeout', '2s', 'rosrun', 'find_object_2d', 'find_object_2d', 'image:=/camera/rgb/image_raw', '>', 'test.txt'])
+        text_file = open("test.txt", "r")
+        count = 0
+        while(True):
+            line = text_file.readline()
+                x = line.split()
+                breakTime = False
+                for i in range (0, len(x)):
+                    if(x[i]=="descriptors"):
+                        if(x[i-1]!="Extracting"):
+                            count= int(x[i-1])
+                            breakTime = True
+                            break
+                if(breakTime == True):
+                    break
+        return count
 
 test = countPoint()
 print(test)
--- patrol.py
+++ patrol.py
@@ -6,6 +6,27 @@ from geometry_msgs.msg import Pose, Point, Quaternion
 import sys
 import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import subprocess
+def countPoint():
+    p = subprocess.run(['timeout', '2s', 'rosrun', 'find_object_2d', 'find_object_2d', 'image:=/camera/rgb/image_raw', '>', 'test.txt'])
+        text_file = open("test.txt", "r")
+        count = 0
+        while(True):
+            line = text_file.readline()
+                x = line.split()
+                breakTime = False
+                for i in range (0, len(x)):
+                    if(x[i]=="descriptors"):
+                        if(x[i-1]!="Extracting"):
+                            count= int(x[i-1])
+                            breakTime = True
+                            break
+                if(breakTime == True):
+                    break
+        return count
+
+test = countPoint()
+print(test)
 
 class navigate():
     def __init__(self):
